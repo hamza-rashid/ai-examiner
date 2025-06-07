5a315dce7ea6d5cfde7367ce9b5789159ec9dee0
Hamza Rashid <hamzarashid720@gmail.com>
2025-06-07 23:16:20 +0100

    updated login

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+console.log("API KEY:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import { AuthProvider } from "./AuthContext";
-import reportWebVitals from "./reportWebVitals";
-import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ChakraProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
-
-reportWebVitals();
